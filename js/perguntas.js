dcb9171efae1a194ef4e74e432d83a48fb892bb2
bele18 <cibele.farias.costa@escola.pr.gov.br>
2025-08-05 01:33:45 +0000

    projeto

---
+++ js/perguntas.js
@@ -0,0 +1,154 @@
+export const perguntas = [
+    {
+        enunciado: "Escolhendo um Livro de Romance",
+        alternativas: [
+            {
+                texto: "Atuais",
+                afirmacao: [
+                    "O livro do amor, Biblioteca das almas",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Passados",
+                afirmacao: [
+                    "Orgulho e preconceito, o duque implacável",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "em romances históricos, você gosta de fantasia / paranormal ou algo mais realista?",
+        alternativas: [
+            {
+                texto: "Fantasia/paranormal",
+                afirmacao: [
+                    "Deusa da fúria, Reino em Brasas, Sussuros na Nablina, Acotar",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Realista",
+                afirmacao: [
+                    "Romance Histórico Tradicional   ",
+                    "Ex: orgulho e preconceito",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Em romances atuais, você prefere histórias mais leves ou dramáticas",
+        alternativas: [
+            {
+                texto: "Leves",
+            afirmacao: [
+                        "Comédia romantica contemporanea",
+                        "Ex um lugar chamado Notting Hill",
+            ],
+                proxima: 3,
+            },
+            {
+                texto: "Dramáticas",
+                afirmacao: [
+                        "Ex Um dia",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual criatura fantástica você prefere?",
+        alternativas: [
+            {
+                texto: "Vampiros",
+                afirmacao: [
+                    "Ex Crepúsculo",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Anjos/Fadas/Ogros",
+                afirmacao: [
+                    "Ex A rainha vermelha",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você gosta de humor leve e divertido",
+        alternativas: [
+            {
+                texto: "Os diários do vampiro",
+                afirmacao: [
+                    "Para da um toque sobrenatural leve.",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Como eu era antes de você",
+                afirmacao: [
+                    "romance leve e emocionante.",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Mundos novos, batalhas épicas, ação rápida, fantasia clássica",
+        alternativas: [
+            {
+                texto: "O Hobbit",
+                afirmacao: [
+                    "J.R.R. Tolkien uma aventura clássica",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Percy Jackson e os Olimpinos",
+                afirmacao: [
+                    "Rick Riordan mistura mitologia grega com muita ação e humor.",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Toque mágico no real, romance sobrenatural, misterioso lento paranormal",
+        alternativas: [
+            {
+                texto: "A hospedeira",
+                afirmacao: [
+                    "Um romance intenso com elementos de ficção cientifica e mistério",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Corte de Espinhos e rosas.",
+                afirmacao: [
+                    "Fantasia com romance profundo.",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Para finais surpreendentes",
+        alternativas: [
+            {
+                texto: "Garota Exemplar",
+                afirmacao: [
+                    "Suspense psicologico com reviravoltas",
+                ],
+            },
+            {
+                texto: "O Ceifador",
+                afirmacao: [
+                    "Fantasia distópica com muitas reflexões e finais inesperados."
+                ],
+            },
+        ]
+    }
+];
